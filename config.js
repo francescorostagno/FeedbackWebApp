ee1d35be9244aa35d00cf13a85cb0f4481f85c06
Francesco Rostagno <francesco@join-italia.com>
2022-07-25 15:50:37 +0200

    correzioni credenziali heroku

--- config.js
+++ config.js
@@ -2,17 +2,13 @@ module.exports = {
     'facebookAuth': {
         'clientID': '1284978548640132', // your App ID
         'clientSecret': '2970a7f130ac5dfef4b4fc5f04165e95', // your App Secret
-        'callbackURL': 'https://legendarysite.herokuapp.com/auth/facebook/callback'
+        'callbackURL': 'https://feedbackwebapp.herokuapp.com/auth/facebook/callback'
     },
     'mysql': {
-        'host': 'us-cdbr-east-04.cleardb.com',
-        'user': 'b80f953880461b',
-        'password': '42d79a45',
-        'database': 'heroku_33058f4afa181ba'
+        'host': 'eu-cdbr-west-03.cleardb.net',
+        'user': 'be591af0c6a000',
+        'password': 'f8243770',
+        'database': 'heroku_1a98a5f1f225d61'
     },
     'use_database': true,
-    'mailgun': {
-        'apiKey' : '60ce1530cbad57f82c984e317238dde3-30b9cd6d-750207e8',
-        'domain': 'sandbox84361e91f22b414bbe59e6bc444b190c.mailgun.org'
-    }
 }
