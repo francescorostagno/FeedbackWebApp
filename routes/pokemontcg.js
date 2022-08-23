bf369f13a619e293b6da1dda77d47dcd36c8e70a
Francesco Rostagno <francesco@join-italia.com>
2022-08-23 09:07:29 +0200

    inseririmento di file readme e delle immagini aggiornate per la sezione info

--- routes/pokemontcg.js
+++ routes/pokemontcg.js
@@ -11,35 +11,62 @@ router.get('/', isLoggedIn, function(req, res, next) {
             userRole = role;
             switch (req.query.search_type){
                 case 'id':
-                    pokemon.card.find(req.query.search_card_name)
-                        .then(card => {
-                            res.redirect('/singlecard?card_id='+req.query.search_card_name);
-                        })
+                    if( req.query.search_card_name !== ''){
+                        pokemon.card.find(req.query.search_card_name)
+                            .then(card => {
+                                res.redirect('/singlecard?card_id='+req.query.search_card_name);
+                            })
+                    }else{
+                        res.render('pokemontcg.twig', {
+                            profile: req.user,
+                            profileUrl: req.user.photos[0].value,
+                            userRole: userRole,
+                            sets: sets
+                        });
+                    }
                     break;
                 case 'name':
-                    pokemon.card.where({ q: 'name:' + req.query.search_card_name })
-                        .then(result => {
-                            res.render('pokemontcg.twig', {
-                                profile: req.user,
-                                profileUrl: req.user.photos[0].value,
-                                data: result.data,
-                                userRole: userRole,
-                                sets: sets
-                            });
-                        })
+                    if(req.query.search_card_name !== ''){
+                        pokemon.card.where({ q: 'name:' + req.query.search_card_name })
+                            .then(result => {
+                                res.render('pokemontcg.twig', {
+                                    profile: req.user,
+                                    profileUrl: req.user.photos[0].value,
+                                    data: result.data,
+                                    userRole: userRole,
+                                    sets: sets
+                                });
+                            })
+                    }else{
+                        res.render('pokemontcg.twig', {
+                            profile: req.user,
+                            profileUrl: req.user.photos[0].value,
+                            userRole: userRole,
+                            sets: sets
+                        });
+                    }
                     break;
                 case 'set':
                     // Filter cards via query parameters
-                    pokemon.card.where({ q: 'set.id:' +   unescape(req.query.search_sets) })
-                        .then(result => {
-                            res.render('pokemontcg.twig', {
-                                profile: req.user,
-                                profileUrl: req.user.photos[0].value,
-                                data: result.data,
-                                userRole: userRole,
-                                sets: sets
-                            });
-                        })
+                    if(req.query.search_sets !== '' ){
+                        pokemon.card.where({ q: 'set.id:' +   unescape(req.query.search_sets) })
+                            .then(result => {
+                                res.render('pokemontcg.twig', {
+                                    profile: req.user,
+                                    profileUrl: req.user.photos[0].value,
+                                    data: result.data,
+                                    userRole: userRole,
+                                    sets: sets
+                                });
+                            })
+                    }else{
+                        res.render('pokemontcg.twig', {
+                            profile: req.user,
+                            profileUrl: req.user.photos[0].value,
+                            userRole: userRole,
+                            sets: sets
+                        });
+                    }
                     break;
                 default:
                     res.render('pokemontcg.twig', {
